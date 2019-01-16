e901cf0158a85cf127de47cbc92a4aaf3ec702a9
Grreg Gorey <greg@bang.net.nz>
2019-01-16 17:00:02 +1300

    Renames project start column to start_date

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_15_191454) do
+ActiveRecord::Schema.define(version: 2019_01_15_214705) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.text "summary"
-    t.date "start"
+    t.date "start_date"
     t.string "slug"
     t.integer "user_id"
     t.datetime "created_at", null: false
