8c734a351788eb67c81d8234513b1f998fc5bbc9
Grreg Gorey <greg@bang.net.nz>
2019-01-30 10:46:15 +1300

    Fixes some rubocop warnings & implements form error in a better way

--- app/helpers/errors_helper.rb
+++ app/helpers/errors_helper.rb
@@ -1,13 +1,4 @@
 module ErrorsHelper
-  def errors_notification(resource)
-    return "" if resource.errors.empty?
-
-    content_tag :div, class: "notification is-warning" do
-      content_tag(:p, sentence) +
-        content_tag(:ul, error_messages(resource))
-    end
-  end
-
   def help_error_message(resource, attribute, default = "")
     has_error = resource.errors.details.include? attribute
     error_msgs = resource.errors.full_messages_for(attribute).join(". ")
@@ -15,12 +6,6 @@ module ErrorsHelper
     return "" unless has_error || default
 
     content_tag :p, has_error ? error_msgs : default, class: "help#{has_error ? " is-danger" : ""}"
-  end
-
-  private
-
-  def error_messages(resource)
-    resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
   end
 
   def errors_count(resource)
