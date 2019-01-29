f3c74b919a3325e6abd79d56d2defb2689af587b
Grreg Gorey <greg@bang.net.nz>
2019-01-29 17:24:56 +1300

    Sets user's timezone when user is created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  after_action :save_user_timezone, only: [:create], if: :devise_controller?
+
+  def browser_time_zone
+    browser_tz = ActiveSupport::TimeZone.find_tzinfo(cookies[:timezone])
+    ActiveSupport::TimeZone.all.find { |zone| zone.tzinfo == browser_tz } || Time.zone
+  rescue TZInfo::UnknownTimezone, TZInfo::InvalidTimezoneIdentifier
+    Time.zone
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:timezone])
+  end
+
+  private
+
+  def save_user_timezone
+    return unless resource.persisted?
+
+    resource.update(timezone: browser_time_zone.name)
+  end
 end
