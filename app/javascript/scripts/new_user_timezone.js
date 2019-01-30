3fb347068e2751073b8e3ce0fdf92f99d25ea4c8
Grreg Gorey <greg@bang.net.nz>
2019-01-30 14:27:23 +1300

    Moves `oldIntl` const inside try/catch

--- app/javascript/scripts/new_user_timezone.js
+++ app/javascript/scripts/new_user_timezone.js
@@ -2,9 +2,8 @@ import jstz from 'jstz';
 import setCookie from './set_cookie';
 
 export default () => {
-  const oldIntl = window.Intl;
-
   try {
+    const oldIntl = window.Intl;
     window.Intl = undefined;
     const tz = jstz.determine().name();
     window.Intl = oldIntl;
