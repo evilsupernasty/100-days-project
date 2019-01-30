ee4f725c0bfc91139190bd07338c392e1a275687
Grreg Gorey <greg@bang.net.nz>
2019-01-30 13:58:28 +1300

    Moves strings in edit user view to translation

--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -1,7 +1,7 @@
 module FormsHelper
-  def minimum_password_length?
+  def minimum_password_length
     return "" unless @minimum_password_length
 
-    "#{@minimum_password_length} characters minimum. "
+    t "shared.form.minimum_password_length", min_pwd_length: @minimum_password_length
   end
 end
