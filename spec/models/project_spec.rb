e49b373802d74c2980f63d962314478aa9d049fc
Grreg Gorey <greg@bang.net.nz>
2019-02-15 15:03:33 +1300

    WIP project specs

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    project = Project.new(title: "Title", start_date: Date.current.to_s(:db), user_id: 1)
+    expect(project).to be_valid
+  end
+
+  it "is not valid without a title" do
+    project = Project.new(title: nil)
+    expect(project).to_not be_valid
+  end
+
+  it "is not valid without a start_date"
 end
